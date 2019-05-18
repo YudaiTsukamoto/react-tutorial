27d273fd48328edd2f94c2902c3d7373ea70aacc
YudaiTsukamoto <yudai.sr400@gmail.com>
2019-05-18 14:35:13 +0900

    型のリファクタ

--- src/modules/users/reducer.ts
+++ src/modules/users/reducer.ts
@@ -1,10 +1,14 @@
-import { AnyAction } from 'redux';
-import { FETCH_USER_START, FETCH_USER_SUCCESS, State } from './types';
+import {
+  FETCH_USER_START,
+  FETCH_USER_SUCCESS,
+  State,
+  UserActionTypes,
+} from './types';
 import { mergeUser } from './selectors';
 
 const initialState: State = [];
 
-export default (state = initialState, action: AnyAction) => {
+export default (state = initialState, action: UserActionTypes) => {
   switch (action.type) {
     case FETCH_USER_START:
       return state;
